b14404a7e08bd9f936e978f9f6b21b9860cc7fa6
kalvin <kalvinvini1998@gmail.com>
2019-11-13 20:14:17 -0300

    sorted array of nodes

--- src/core/helpers.c
+++ src/core/helpers.c
@@ -1,25 +1,35 @@
 #include "../include/helpers.h"
 
+int comparator(const void *a, const void *b)
+{
+    const Node* const * elem1 = a;
+    const Node* const * elem2 = b;
 
-Node** getFrequency(const char* text){
+    return (*elem2)->frequency - (*elem1)->frequency; // Beware of overflow / underflow
+     
+}
 
+Node **getFrequency(const char *text)
+{
 
-    Node** frequency = (Node**)malloc(26 * sizeof(Node*));
+    Node **frequency = (Node **)malloc(26 * sizeof(Node *));
 
-    for(int i = 0; i < 26; i++) frequency[i] = createNode();
+    for (int i = 0; i < 26; i++)
+        frequency[i] = createNode();
 
     int size = strlen(text);
 
-    for(int i = 0; i < size; i++){
-        if(text[i] != ' '){
+    for (int i = 0; i < size; i++)
+    {
+        if (text[i] != ' ')
+        {
             int current = tolower(text[i]) - 97;
             frequency[current]->frequency++;
             frequency[current]->letter = tolower(text[i]);
         }
     }
 
+    qsort(frequency, 26 , sizeof(struct Node *), comparator);
+
     return frequency;
-
 }
-
-
